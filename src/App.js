37aa1e9592a4a12289270f977cda35128811bb29
Rahul Ponnuru <sairahulponnuru3@gmail.com>
2023-06-10 20:14:59 +0530

    Changes Done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Adipurush
+          Jai shri ram
         </p>
         <a
           className="App-link"
